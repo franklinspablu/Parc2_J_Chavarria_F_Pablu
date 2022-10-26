891f95e591f3eec6c324f76fb3c7c0162b59d55d
franklinspablu <franklinspablu@gmail.com>
2022-10-26 10:56:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/parc2_j_chavarria_f_pablu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parc2_j_chavarria_f_pablu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
