b012fc35e05512c8345bdb0ca8a5e995b17b874a
JuanjoChavarria <juan.chavarria@utp.ac.pa>
2022-10-27 22:00:36 -0500

    Iconos de Usuario y contraseña agregados. creacion de clase ManejoDeArchivos para crear los metodos que crearan y leeran los registros.

---
+++ app/src/main/java/com/example/parc2_j_chavarria_f_pablu/Helpers/ManejoDeArchivos.java
@@ -0,0 +1,12 @@
+package com.example.parc2_j_chavarria_f_pablu.Helpers;
+
+public class ManejoDeArchivos {
+
+
+    public void WriteInFile(String x){
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/parc2_j_chavarria_f_pablu/MainActivity.java
+++ app/src/main/java/com/example/parc2_j_chavarria_f_pablu/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.parc2_j_chavarria_f_pablu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText txtCedula, txtContrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+
+
+
 }
